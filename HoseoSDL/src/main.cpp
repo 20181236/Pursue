60c9a7c31be546506eb86682d5216fe9b0c02a32
20181236 <20181236@vision.hoseo.edu>
2022-05-09 16:22:41 +0900

    2차 test
    2차

--- HoseoSDL/src/main.cpp
+++ HoseoSDL/src/main.cpp
@@ -29,6 +29,7 @@ void render();
 int main(int argc, char* argv[])
 {
     //github test
+    //github test2
     if (!init()) {
 			return 1; // something's wrong
     }
@@ -48,7 +49,7 @@ int main(int argc, char* argv[])
     SDL_Color color_Blue = { 0, 0, 255 };
     SDL_Color color_White = { 255, 255, 255 };
 #ifdef UNICODE
-    SDL_Surface* temp_surf = TTF_RenderUNICODE_Shaded(m_pFont, (Uint16*)L"텍스트 텍스쳐", color_Blue, color_White);
+    SDL_Surface* temp_surf = TTF_RenderUNICODE_Shaded(m_pFont, (Uint16*)L"한글 텍스쳐", color_Blue, color_White);
 #else
     SDL_Surface* temp_surf = TTF_RenderUTF8_Shaded(m_pFont, "한글 텍스쳐", color_Blue, color_White);
 #endif
@@ -182,7 +183,7 @@ void render()
     SDL_RenderCopy(g_pRenderer, texureImage, &rectImage, &rectImage);
     SDL_RenderCopy(g_pRenderer, texureText, &rectText, &rectText);
 
-    filledCircleColor(g_pRenderer, 300, 300, 50, 0xFFFF0000);
+    filledCircleColor(g_pRenderer, 300, 300, 50, 0xFFFFFF00);
 
     SDL_RenderPresent(g_pRenderer);
 }
